4aae47d279a1b07ca4490e9af289a5e1e7173bb0
Jordi <jordilop@gmail.com>
2023-11-20 17:40:29 +0100

    added form + material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'activitat6-angular';
+  nom: FormControl = new FormControl('', Validators.required);
+  email: FormControl = new FormControl('', [Validators.required, Validators.email]);
+  telefon: FormControl = new FormControl('', Validators.required);
+  missatge: FormControl = new FormControl('', Validators.required);
+
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      nom: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      telefon: ['', [Validators.required, Validators.minLength(9)]],
+      missatge: ['', [Validators.required, Validators.minLength(20)]],
+    })
+  }
+
+  sendValues() {
+    alert(`
+    DADES FORMULARI:
+    Nom: ${this.form.value.nom}
+    Email: ${this.form.value.email}
+    Telèfon: ${this.form.value.telefon}
+    Missatge: ${this.form.value.missatge}
+    `);
+    this.form.reset();
+  }
 }
